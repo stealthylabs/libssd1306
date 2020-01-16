138d6c1e6355263292255b6ed2514e483e345284
Stealthy Labs <github@stealthylabs.com>
2020-01-16 01:47:29 +0000

    The init/free functions now are open/close() functions on the device and the ioctl()s are set on open().

--- ssd1306_i2c.c
+++ ssd1306_i2c.c
@@ -17,7 +17,7 @@
 #include <ssd1306_i2c.h>
 
 
-ssd1306_i2c_t *ssd1306_i2c_init(
+ssd1306_i2c_t *ssd1306_i2c_open(
         const char *dev, // name of the device such as /dev/i2c-1. cannot be NULL
         uint8_t addr, // I2C address of the device. valid values: 0 (default) or 0x3c or 0x3d
         uint8_t height, // OLED display height. valid values: 0 (default) or 128
@@ -25,29 +25,42 @@ ssd1306_i2c_t *ssd1306_i2c_init(
     )
 {
     ssd1306_i2c_t *ptr = NULL;
+    int rc = 0;
     do {
         if (!dev) {
             fprintf(stderr, "ERROR: No device given.\n");
+            rc = -1;
             break;
         }
         ptr = calloc(1, sizeof(*ptr));
         if (!ptr) {
             fprintf(stderr, "ERROR: Failed to allocate memory of size %zu bytes\n", sizeof(*ptr));
+            rc = -1;
             break;
         }
+        ptr->err.errstr = calloc(1, 256);
+        if (!ptr->err.errstr) {
+            fprintf(stderr, "ERROR: Failed to allocate memory of size 256 bytes\n");
+            rc = -1;
+            break;
+        }
+        ptr->err.errstr_max_len = 256;
         ptr->dev = strdup(dev);
         if (!ptr->dev) {
-            ptr->last_err = errno;
-            fprintf(stderr, "WARN: Failed to copy device name: %s. Ignoring potential memory error: %s\n", dev, strerror(ptr->last_err));
+            ptr->err.errnum = errno;
+            strerror_r(ptr->err.errnum, ptr->err.errstr, ptr->err.errstr_max_len);
+            fprintf(stderr, "WARN: Failed to copy device name: %s. Ignoring potential memory error: %s\n", dev, ptr->err.errstr);
         } else {
-            ptr->last_err = 0;
+            ptr->err.errnum = 0;
+            memset(ptr->err.errstr, 0, ptr->err.errstr_max_len);
+            strerror_r(ptr->err.errnum, ptr->err.errstr, ptr->err.errstr_max_len);
         }
         if (addr == 0x3c || addr == 0) {
             ptr->addr = 0x3c;
         } else if (addr == 0x3d) {
             ptr->addr = 0x3d;
         } else {
-            fprintf(stderr, "WARN: I2C device addr cannot be %x. Using 0x3c\n",
+            fprintf(stderr, "WARN: I2C device addr cannot be 0x%x. Using 0x3c\n",
                     addr);
             ptr->addr = 0x3c;
         }
@@ -61,18 +74,52 @@ ssd1306_i2c_t *ssd1306_i2c_init(
                     width, 32);
             ptr->width = 32;
         }
-        ptr->fd = -1;
+        ptr->fd = open(dev, O_RDWR);
+        if (ptr->fd < 0) {
+            ptr->err.errnum = errno;
+            strerror_r(ptr->err.errnum, ptr->err.errstr, ptr->err.errstr_max_len);
+            fprintf(stderr, "ERROR: Failed to open %s in read/write mode: %s\n",
+                    dev, ptr->err.errstr);
+            rc = -1;
+            break;
+        } else {
+            fprintf(stderr, "INFO: Opened %s at fd %d\n", dev, ptr->fd);
+            uint32_t addr = (uint32_t)ptr->addr;
+            if (ioctl(ptr->fd, I2C_SLAVE, addr) < 0) {
+                ptr->err.errnum = errno;
+                strerror_r(ptr->err.errnum, ptr->err.errstr, ptr->err.errstr_max_len);
+                fprintf(stderr, "ERROR: Failed to set I2C_SLAVE for %s addr 0x%x: %s\n",
+                        dev, addr, ptr->err.errstr);
+                rc = -1;
+                break;
+            } else {
+                fprintf(stderr, "INFO: I2C_SLAVE for %s addr 0x%x opened in RDWR mode\n",
+                        dev, addr);
+                rc = 0;
+            }
+        }
     } while (0);
+    if (rc < 0) {
+        ssd1306_i2c_close(ptr);
+        ptr = NULL;
+    }
     return ptr;
 }
 
-void ssd1306_i2c_free(ssd1306_i2c_t *ptr)
+void ssd1306_i2c_close(ssd1306_i2c_t *ptr)
 {
     if (ptr) {
+        if (ptr->fd > 0) {
+            close(ptr->fd);
+        }
         if (ptr->dev) {
             free(ptr->dev);
         }
         ptr->dev = NULL;
+        if (ptr->err.errstr) {
+            free(ptr->err.errstr);
+            ptr->err.errstr = NULL;
+        }
         memset(ptr, 0, sizeof(*ptr));
         free(ptr);
         ptr = NULL;
--- ssd1306_i2c.h
+++ ssd1306_i2c.h
@@ -10,21 +10,27 @@
 #include <stdint.h>
 
 typedef struct {
+    int errnum; // store the errno here
+    char *errstr; // error string, allocated on the heap
+    size_t errstr_max_len; // size of the error string buffer
+} ssd1306_i2c_err_t;
+
+typedef struct {
     int fd;
-    int last_err;
     char *dev;      // device name. a copy is made.
     uint8_t addr;   // default 0x3c
     uint8_t height; // default 128
     uint8_t width;  // default 32
+    ssd1306_i2c_err_t err; // for re-entrant error handling
 } ssd1306_i2c_t;
 
-ssd1306_i2c_t *ssd1306_i2c_init(
+ssd1306_i2c_t *ssd1306_i2c_open( // open the device for read/write
         const char *dev, // name of the device such as /dev/i2c-1. cannot be NULL
         uint8_t daddr, // I2C address of the device. valid values: 0 (default) or 0x3c or 0x3d
         uint8_t height, // OLED display height. valid values: 0 (default) or 128
         uint8_t width // OLED display width. valid values: 0 (default), 32 or 64
     );
 
-void ssd1306_i2c_free(ssd1306_i2c_t *); // free object
+void ssd1306_i2c_close(ssd1306_i2c_t *); // free object and close fd
 
 #endif /* __LIB_SSD1306_I2C_H__ */
