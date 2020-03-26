da543a6178df521dc9539f0cbfabeb03419d9ab9
Stealthy Labs <github@stealthylabs.com>
2020-03-25 21:39:13 -0400

    Draw line on the OLED screen too

--- examples/i2c_128x32_graphics.c
+++ examples/i2c_128x32_graphics.c
@@ -71,6 +71,12 @@ int main ()
     ssd1306_framebuffer_bitdump(fbp);
     ssd1306_i2c_display_update(oled, fbp);
     sleep(3);
+    ssd1306_framebuffer_clear(fbp);
+    ssd1306_i2c_display_clear(oled);
+    ssd1306_framebuffer_draw_line(fbp, 0, 0, 64, 32, true);
+    ssd1306_framebuffer_bitdump(fbp);
+    ssd1306_i2c_display_update(oled, fbp);
+    sleep(3);
     ssd1306_i2c_run_cmd(oled, SSD1306_I2C_CMD_POWER_OFF, 0, 0);
     ssd1306_framebuffer_destroy(fbp);
     fbp = NULL;
